12f6308a9b1fdcf0376080c32a171dcba33d7273
Fred Ludlow <fred.ludlow@gmail.com>
2022-03-16 22:13:38 +0000

    Add @rollup/plugin-commonjs plugin to fix build problems with non ES module deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
 import buble from '@rollup/plugin-buble';
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
     input: 'index.js',
     plugins: [
         resolve(),
+	commonjs(), 
         buble()
     ],
     output: {file: 'dist/index.js', format: 'umd'}
